b970855f65bd8ed9e8a44040fc6e91a83a35483b
Parfait Tapsoba <leneodepart@gmail.com>
2021-06-22 14:23:58 +0000

    fixing data no keeping in database again

--- studentData/saveStudentData.php
+++ studentData/saveStudentData.php
@@ -5,20 +5,20 @@
     $con = connectDB();
 
     //Take the informations 
-    $nom = $_POST["nom"];
-    $prenoms = $_POST["prenoms"];
+    $nom = strval($_POST["nom"]);
+    $prenoms = strval($_POST["prenoms"]);
     $date_naissance = $_POST["date_naissance"];
-    $genre = $_POST["genre"];
-    $type_bac = $_POST["type_bac"];
-    $maths_classe = $_POST["maths_classe"];
-    $physique_classe = $_POST["physique_classe"];
-    $svt_classe = $_POST["svt_classe"];
-    $maths_bac = $_POST["maths_bac"];
-    $physique_bac = $_POST["physique_bac"];
-    $svt_bac = $_POST["svt_bac"];
-    $pays = $_POST["pays"];
-    $quartier = $_POST["quartier"];
-    $lycee = $_POST["lycee"];
+    $genre = strval($_POST["genre"]);
+    $type_bac = strval($_POST["type_bac"]);
+    $maths_classe = strval($_POST["maths_classe"]);
+    $physique_classe = strval($_POST["physique_classe"]);
+    $svt_classe = strval($_POST["svt_classe"]);
+    $maths_bac = strval($_POST["maths_bac"]);
+    $physique_bac = strval($_POST["physique_bac"]);
+    $svt_bac = strval($_POST["svt_bac"]);
+    $pays = strval($_POST["pays"]);
+    $quartier = strval($_POST["quartier"]);
+    $lycee = strval($_POST["lycee"]);
     // //Store informations in the DB
     $sql_req = "INSERT INTO `ETUDIANT_TEST` (nom, prenoms, date_naissance, genre, type_bac, maths_classe, physique_classe, svt_classe, maths_bac, physique_bac, svt_bac, pays, quartier, lycee) VALUES ('$nom', '$prenoms', '$date_naissance', '$genre', '$type_bac', '$maths_classe', '$physique_classe', '$svt_classe', '$maths_bac', '$physique_bac', '$svt_bac', '$pays', '$quartier', '$lycee')";
     
